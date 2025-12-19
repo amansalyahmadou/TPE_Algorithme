a109933a27068a298890a66956814dbec273bc65
amansalyahmadou <amansalyahmadou08@gmail.com>
2025-12-19 01:10:39 +0100

    Ajout de main.c, bibliotheque.cet bibliotheque.h

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "bibliotheque.h"
+
+int main( void ) {
+ int result = power( 2, 3 );
+ printf( "2³ == %d\n", result );
+ result = fact( 6 );
+ printf( "6! == %d\n", result );
+
+ return EXIT_SUCCESS;
+}
